efaae716f292fac5af357927a7df796f183d8138
jmmonteiro <joao.monteiro@ucl.ac.uk>
2016-02-17 22:00:17 +0000

    Added more vim functionality

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 here=$PWD
 
+#--- Make links in the home folder to the dot files in this folder
 for file in  ".vimrc"; 
 do
     if [[ $(readlink -f $HOME/$file) != $(readlink -f $here/$file) ]]; then
@@ -9,3 +10,17 @@ do
         echo "linked $file"
     fi
 done
+
+#--- Vim
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Clone the repos with vim plugins
+git clone https://github.com/vim-scripts/Align.git ~/.vim/bundle/Align
+git clone https://github.com/LaTeX-Box-Team/LaTeX-Box.git ~/.vim/bundle/Latex-Box
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
