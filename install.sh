8ef23492a722605e0b13ca8d546c850014735b29
jmmonteiro <joao.monteiro@ucl.ac.uk>
2016-02-17 22:29:05 +0000

    Should have been the last vim plugins added

--- install.sh
+++ install.sh
@@ -24,3 +24,5 @@ git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcomm
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/vim-scripts/SearchComplete.git ~/.vim/bundle/SearchComplete
+git clone https://github.com/vim-scripts/TaskList.vim ~/.vim/bundle/TaskList.vim
