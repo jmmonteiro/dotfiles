cdc32f6006a8ec91c87b9e1945219539e78a5dc6
jmmonteiro <joao.andre.monteiro@gmail.com>
2020-05-05 18:55:52 +0100

    Bugfix: set org directory before using it

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,5 +1,8 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+;; This directory has to be set before org is loaded
+(setq org-directory "~/Dropbox/org/")
+;; Load my configs
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
