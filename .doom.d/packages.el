bf165e6ef945ae3d4e4899754168bda61323337a
jmmonteiro <jmmonteiro@email.com>
2020-06-11 21:20:50 +0100

    Minor changes

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -20,6 +20,7 @@
 
 ; Display
 (package! highlight-indent-guides)
+(package! emojify)
 
 ; Org-mode
 (package! org-super-agenda)
@@ -38,11 +39,11 @@
 (package! racer)
 (package! company-racer)
 
-
 ; Elfeed
 (package! elfeed)
 (package! elfeed-org)
 (package! elfeed-goodies)
+
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
